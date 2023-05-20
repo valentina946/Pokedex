5e43a87fa851a2b5e1aa6e48c6068b7b4e8b38f6
valentina946 <valentina.olariaga@hotmail.com>
2023-05-19 21:01:27 -0300

    Add xcode project

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Valentina Olariaga on 19/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
